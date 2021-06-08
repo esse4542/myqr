2670ba2c22ee2844ef660b784fcdbbd3ee1a0039
Nam <esse4542@nate.com>
2021-06-08 14:47:40 +0900

    QR스캔 projec
    - 가상의 핸드폰으로 qr스캔 가능

--- app/src/main/java/com/example/myqr/MainActivity.java
+++ app/src/main/java/com/example/myqr/MainActivity.java
@@ -12,19 +12,19 @@ import com.google.zxing.integration.android.IntentResult;
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    protected void onCreate(Bundle savedInstanceState) { //  main 매소드
+        super.onCreate(savedInstanceState); // onCreate 매소드를 호출 -> 실행 -> 오버라이드된 매소드 처리
         // setContentView(R.layout.activity_main);
-        new IntentIntegrator(this).initiateScan(); // 객체를 통해 화면을 스캔
+        new IntentIntegrator(this).initiateScan(); // 객체를 통해 화면을 스캔 (qr 리더기 생성)
     }
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
-        if (result != null) {
-            if (result.getContents() == null) {
+        if (result != null) { // result 널이 아니면
+            if (result.getContents() == null) { // null이면 Cancelled 출력 함.
                 Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show();
                 // todo
-            } else {
+            } else { // null이 아니면 결과값 출력
                 Toast.makeText(this, "Scanned: " + result.getContents(), Toast.LENGTH_LONG).show();
                 // todo
             }
